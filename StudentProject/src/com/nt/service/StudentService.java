192452b8818daf010af769df6d58580d423d566b
dev1 <dev1@gmail.com>
2019-04-03 00:16:03 +0530

    dev1 developer

--- StudentProject/src/com/nt/service/StudentService.java
+++ StudentProject/src/com/nt/service/StudentService.java
@@ -1,5 +1,13 @@
 package com.nt.service;
 
 public class StudentService {
+	
+int id=101;
+String name="venu";
+String addrs="hyderabad";
+@Override
+public String toString() {
+	return "StudentService [id=" + id + ", name=" + name + ", addrs=" + addrs + "]";
+}
 
 }
