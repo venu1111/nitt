a83536d6a4a435004221ab6ccd1c7982a103679f
dev2 <dev2@gmail.com>
2019-04-03 00:29:23 +0530

    dev2 test

---
+++ StudentProject/src/com/nt/test/StudentTest.java
@@ -0,0 +1,13 @@
+package com.nt.test;
+
+import com.nt.service.StudentService;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		StudentService service=new StudentService();
+		System.out.println(service);
+
+	}
+
+}
